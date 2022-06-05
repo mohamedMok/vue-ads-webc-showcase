ad28e234f67916b5f5991ac8cbb222dbb8738ba3
mohamedMok <mohamed.mokhtari.pro@gmail.com>
2022-06-05 18:23:16 +0200

    feat(init): add basic config ads web component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <m-button label="Welcome Mohamed"></m-button>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@mozaic-ds/mozaic-web-components/public/adeo/bundle.js";
+import "@mozaic-ds/mozaic-web-components/public/icons.js";
 
 createApp(App).mount('#app')
